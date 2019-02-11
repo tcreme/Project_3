b239b35832fc61798200d148496e592831076ef7
Trini <trcreme@gmail.com>
2019-02-11 11:16:18 -0500

    Modal populates after player crosses water. Indentation was corrected.

--- js/app.js
+++ js/app.js
@@ -2,58 +2,57 @@ let numberOfLives = 3;
 
 // Enemies our player must avoid
 let Enemy = function Enemy (x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.x = x;
-    this.y = y + 55; //center
-    this.speed = speed;
-    this.sprite = 'images/enemy-bug.png';
-    this.step = 101;
-    this.boundry = this.step * 5;
-    this.resetPosition = this.step - 200;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.x = x;
+  this.y = y + 55; //center
+  this.speed = speed;
+  this.sprite = 'images/enemy-bug.png';
+  this.step = 101;
+  this.boundry = this.step * 6;
+  this.resetPosition = this.step - 200;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 
-// if enemy is not passed boundry
-    if (this.x < this.boundry) {
-      // Move forward
-      //increment x by speed * dt
-      this.x += this.speed * dt;
-    }
-    else {
-      //reset pos to start
-      this.x = this.resetPosition;
-      this.speed = (1 + Math.random()) * 180;
-    }
+  // if enemy is not passed boundry
+  if (this.x < this.boundry) {
+    // Move forward
+    //increment x by speed * dt
+    this.x += this.speed * dt;
+  }
+  else {
+    //reset pos to start
+    this.x = this.resetPosition;
+    this.speed = (1 + Math.random()) * 180;
+  }
 };
 
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 
 // Now write your own player class
 let Player = function Player() {
-    this.sprite = 'images/char-cat-girl.png';
-    this.step = 101;
-    this.jump = 83;
-    this.startX = this.step * 2;
-    this.startY = (this.jump * 4) + 55;
-    this.x = this.startX;
-    this.y = this.startY;
-    this.winner = false;
-
+  this.sprite = 'images/char-cat-girl.png';
+  this.step = 101;
+  this.jump = 83;
+  this.startX = this.step * 2;
+  this.startY = (this.jump * 4) + 55;
+  this.x = this.startX;
+  this.y = this.startY;
+  this.winner = false;
 };
 
 
@@ -69,10 +68,10 @@ Player.prototype.update = function() {
     }
   }
   // Check winner here?
-    // Was the final tile reached?
-    if(this.y === 55) {
-      this.winner = true;
-    }
+  // Was the final tile reached?
+  if(this.y <= 50) {
+    this.winner = true;
+  }
 };
 
 
@@ -85,36 +84,39 @@ Player.prototype.render = function() {
 // Reset player
 Player.prototype.reset = function(){
   // Set x and y to their starting points
-    this.y = this.startY;
-    this.x = this.startX;
+  this.y = this.startY;
+  this.x = this.startX;
 }
 
 
 // a handleInput() method.
 Player.prototype.handleInput = function(input) {
 
-    switch (input) {
-      case 'left':
-        if (this.x > 0) {
-            this.x -= this.step;
-        }
-        break;
-      case 'up':
-        if (this.y > this.jump) {
-            this.y -= this.jump;
-        }
-        break;
-      case 'right':
-        if (this.x < this.step * 4) {
-            this.x += this.step;
-        }
-        break;
-      case 'down':
-        if (this.y < this.jump * 4) {
-            this.y += this.jump;
-        }
-        break;
-    }
+  switch (input) {
+    case 'left':
+      if (this.x > 0) {
+        this.x -= this.step;
+      }
+      break;
+
+    case 'up':
+      if (this.y > 0) {
+        this.y -= this.jump;
+      }
+      break;
+
+    case 'right':
+      if (this.x < this.step * 4) {
+        this.x += this.step;
+      }
+      break;
+
+    case 'down':
+      if (this.y < this.jump * 4) {
+        this.y += this.jump;
+      }
+      break;
+  }
 }
 
 // Now instantiate your objects.
@@ -134,28 +136,29 @@ window.player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    let allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  let allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
 
 function showLives(){
   let charDOM = document.querySelector('.character');
-    if (numberOfLives == 3) {
-      charDOM.innerHTML = `<li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>
-                          <li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>
-                          <li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>`;
-    } else if (numberOfLives == 2) {
-      charDOM.innerHTML = `<li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>
-                          <li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>`
-    } else if (numberOfLives == 1) {
-      charDOM.innerHTML = `<li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>`
-    } else {
-        charDOM.innerHTML = ``;
-    }
+
+  if (numberOfLives == 3) {
+    charDOM.innerHTML = `<li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>
+                        <li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>
+                        <li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>`;
+  } else if (numberOfLives == 2) {
+    charDOM.innerHTML = `<li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>
+                        <li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>`
+  } else if (numberOfLives == 1) {
+    charDOM.innerHTML = `<li><i class = "iconCharacter"></i><img src="images/char-cat-girl.png"></li>`
+  } else {
+    charDOM.innerHTML = ``;
+  }
 }
